7278730a44fda167c00d9c4506fc2ed43f2b9640
Ravi <ravisharmarock007@gmail.com>
2025-06-08 03:46:42 +0530

    Add new code logic
    Added new code logic that prevents
    -repeated operator use
    -repeated equal use
    -updates previous values on memory panel

--- script.js
+++ script.js
@@ -1,128 +1,131 @@
-let prevVal=null;
-let DataOnDisp="";
-let DataOnMemoryPanel="";
-let prevOperator=null;
-let inputNum="";
-let currentOperator="";
+let DataOnDisp = "";
+let DataOnMemoryPanel = "";
+let prevBtnIsEquals = false;
+let lastBtnIsOp = false;
 
-const btnBox=document.querySelector(".btnBox");
-const displayPanel=document.querySelector(".displayPanel");
-const memoryPanel=document.querySelector(".displayMemory");
+let val1 = null,
+  val2 = null,
+  op = null;
+let result;
 
-function checkBtnIsNum(e)
-{
-    return (e.target.id.slice(0,3)==="Num");
+const btnBox = document.querySelector(".btnBox");
+const displayPanel = document.querySelector(".displayPanel");
+const memoryPanel = document.querySelector(".displayMemory");
+
+function checkBtnIsNum(e) {
+  return e.target.id.slice(0, 3) === "Num";
 }
-function checkBtnIsOperator(e)
-{
-     return (e.target.id.slice(0,2)==="OP");
+function checkBtnIsOperator(e) {
+  return e.target.id.slice(0, 2) === "OP";
 }
-function getBtnValue(e)
-{
-   return e.target.textContent;
+function getBtnValue(e) {
+  return e.target.textContent;
 }
-function getOperatorType(e)
-{
-    return e.target.textContent;
+function getOperatorType(e) {
+  return e.target.textContent;
 }
 
-function clearDisplayPanel()
-{
-    displayPanel.textContent="";
-    DataOnDisp="";
+function clearDisplayPanel() {
+  displayPanel.textContent = "";
+  DataOnDisp = "";
 }
-function clearMemoryPanel()
-{
-     memoryPanel.textContent="";
-    DataOnMemoryPanel="";
+function clearMemoryPanel() {
+  memoryPanel.textContent = "";
+  DataOnMemoryPanel = "";
 }
-function clearOldValue()
-{
-    prevVal=null;
-    prevOperator=null;
+function resetValue() {
+  val1 = null;
+  val2 = null;
+  op = null;
 }
-function Calculate(e)
-{
-    if(checkBtnIsNum(e))  //if clicked btn is a numeric btn
-    {
-        if(prevOperator==="=")
-            clearMemoryPanel();
-        DataOnDisp+=getBtnValue(e);
-        displayPanel.textContent=DataOnDisp;
+function getValueFromDU() {
+  return displayPanel.textContent;
+}
+function calculateResult() {
+  result = +operate(val1, val2, op);
+  if (result % 1 !== 0) result = result.toFixed(2);
+  console.log(`${val1} ${op} ${val2} = ${result}`);
+}
+
+function Calculate(e) {
+  if (checkBtnIsNum(e)) {
+    //if clicked btn is a numeric btn
+    if (prevBtnIsEquals) {
+      clearMemoryPanel();
+      prevBtnIsEquals = false;
     }
-    else if(checkBtnIsOperator(e))   //if clicked btn is an operator
-    {
-        inputNum=+displayPanel.textContent;
-        currentOperator=getOperatorType(e);
-        if(prevOperator===null)
-            prevOperator=currentOperator;
-        DataOnMemoryPanel+=DataOnDisp+currentOperator;
-        memoryPanel.textContent=DataOnMemoryPanel;
-        clearDisplayPanel();
-        // console.log(`Previous Operator=${prevOperator}`);
-        // console.log(`Current Operator=${currentOperator}`);
-           if(prevVal===null)
-            {
-                prevVal=inputNum;
-            }
-           else
-            {
-                prevVal=+operate(prevVal,inputNum,prevOperator);
-                if(prevVal%1!==0)
-                    prevVal=prevVal.toFixed(2);
-                displayPanel.textContent=prevVal;
-            }
-           if(currentOperator==="=")
-           {    
-                DataOnMemoryPanel=prevVal;
-                clearOldValue();
-           }
-           prevOperator=currentOperator;
+    if (lastBtnIsOp && op !== null && val1 != null) DataOnMemoryPanel += op;
+    lastBtnIsOp = false;
+    DataOnDisp += getBtnValue(e);
+    displayPanel.textContent = DataOnDisp;
+  }
+  if (checkBtnIsOperator(e)) {
+    if (prevBtnIsEquals) {
+      clearMemoryPanel();
+      prevBtnIsEquals = false;
     }
-    else if(e.target.id==="btnClear")
-        {
-            clearDisplayPanel();
-            clearMemoryPanel();
-            clearOldValue();
-        }
+
+    if (getValueFromDU() != "" && lastBtnIsOp != true) {
+      DataOnMemoryPanel += getValueFromDU();
+      if (val1 != null) val2 = +getValueFromDU();
+      else val1 = +getValueFromDU();
+      clearDisplayPanel();
+      if (val1 != null && val2 != null && op != null) {
+        console.log("entered");
+        calculateResult();
+        val1 = result;
+        displayPanel.textContent = result;
+      }
+    }
+    op = getOperatorType(e);
+    lastBtnIsOp = true;
+  }
+  if (e.target.id === "btnEqual") {
+    prevBtnIsEquals = true;
+    if (val1 != null && getValueFromDU() != "") val2 = +getValueFromDU();
+    if (val1 != null && val2 != null && op != null) {
+      DataOnMemoryPanel += getValueFromDU();
+      clearDisplayPanel();
+      calculateResult();
+      resetValue();
+      displayPanel.textContent = result;
+    }
+    op = null;
+  } else if (e.target.id === "btnClear") {
+    clearMemoryPanel();
+    clearDisplayPanel();
+    resetValue();
+  }
+  memoryPanel.textContent = DataOnMemoryPanel;
 }
 
+btnBox.addEventListener("click", Calculate);
 
-
-
-
-btnBox.addEventListener("click",Calculate);
-
-
-
-
-        function add(a,b)
-            {
-                return a+b;
-            }
-        function sub(a,b)
-            {
-                return a-b;
-            }
-        function mult(a,b)
-            {
-                return a*b;
-            }
-        function div(a,b)
-            {
-                return (a/b).toFixed(2);
-            }    
-        function operate(a,b,operator)
-            {
-                switch(operator)
-                {
-                    case '+': return(add(a,b));
-                              break;
-                    case '-': return(sub(a,b));
-                              break;
-                    case '*': return(mult(a,b));
-                              break;
-                    case '/': return(div(a,b));
-                              break;
-                }
-            }
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function mult(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return (a / b).toFixed(2);
+}
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return sub(a, b);
+      break;
+    case "*":
+      return mult(a, b);
+      break;
+    case "/":
+      return div(a, b);
+      break;
+  }
+}
